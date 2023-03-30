ab8e6b07ef8a74c031f676c07bd8caa74af6c549
prathamupadhyay-tech <pratham27900@gmail.com>
2023-03-31 00:00:12 +0530

    Data set added

--- inference_classifier.py
+++ inference_classifier.py
@@ -15,7 +15,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-labels_dict = {0: 'A', 1: 'B', 2: 'C'}
+labels_dict = {0: 'A', 1: 'B', 2: 'C', 3:'D' , 4:'E',5:'F' ,6:'G' , 7:'H' , 8: 'I' ,9:'K' , 10:'L' , 11:'M' ,12: 'N' ,13: 'O' , 14: 'P' ,15:'Q' ,16:'R' , 17: 'S' ,18: 'T' , 19:'U' ,20:'V' ,21: 'W' , 22:'X' , 23:'Y'  }
 while True:
 
     data_aux = []
