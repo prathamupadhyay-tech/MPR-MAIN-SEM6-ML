8ed4891c4c0220a1ee73f2252767277b48e2e689
Adhiraj10 <soparkaradhiraj@gmail.com>
2023-04-01 15:13:22 +0530

    landmarks showing up

--- inference_classifier.py
+++ inference_classifier.py
@@ -34,6 +34,14 @@ while True:
     results = hands.process(frame_rgb)
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                frame,  # image to draw
+                hand_landmarks,  # model output
+                mp_hands.HAND_CONNECTIONS,  # hand connections
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+
+        for hand_landmarks in results.multi_hand_landmarks:
             data_aux = []
             x_ = []
             y_ = []
